59aec1158b81f714423b67d7412a477d2b20ddc1
mohammedsalman14 <mohamedsalman140@gmail.com>
2025-08-08 11:41:53 +0530

    Add file.groovy for Jenkins pipeline

--- Jenkins/file.groovy
+++ Jenkins/file.groovy
@@ -1,13 +1,14 @@
 def build() {
-    echo "Building the project..."
+    echo "Building the application..."
 }
 
 def test() {
-    echo "Running tests..."
+    echo "Testing the application..."
 }
 
 def deploy() {
-    echo "Deploying the project..."
+    echo "Deploying the application..."
 }
 
+// Expose the methods to Jenkinsfile
 return this
